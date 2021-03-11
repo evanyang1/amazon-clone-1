65fc9ce37b86a4edce6c77d6e62ea624b7d8ead5
Evan Yang <evanjyang4@gmail.com>
2021-03-11 14:59:14 -0800

    nav styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header.js'
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
