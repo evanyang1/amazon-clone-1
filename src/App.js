94b89d23f9a1500c82ba48f9f761db54fe813ac3
Evan Yang <evanjyang4@gmail.com>
2021-03-11 01:26:53 -0800

    30:19

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA_8qKm93ZtbHF-12-9ywxWjteNT9NlYG0",
+    authDomain: "clone-7f67f.firebaseapp.com",
+    projectId: "clone-7f67f",
+    storageBucket: "clone-7f67f.appspot.com",
+    messagingSenderId: "7265893108",
+    appId: "1:7265893108:web:57738e55e61277a0783ca1",
+    measurementId: "G-K9N5X35VPK"
+  };
