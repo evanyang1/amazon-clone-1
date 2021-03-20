edda92c437fe3fa68b1e3e5bc88ef6be7f921ab3
Evan Yang <evanjyang4@gmail.com>
2021-03-20 14:45:10 -0700

    amazon checkout ui

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,13 +4,13 @@ import CurrencyFormat from "react-currency-format";
 
 function Subtotal() {
     return (
-        <div>
+        <div className="subtotal">
             <CurrencyFormat
                 renderText={(value) => (
                 <>
                     <p>
                     {/* Part of the homework */}
-                    Subtotal ({basket.length} items): <strong>{value}</strong>
+                    Subtotal (0 items): <strong>0</strong>
                     </p>
                     <small className="subtotal__gift">
                     <input type="checkbox" /> This order contains a gift
@@ -18,11 +18,12 @@ function Subtotal() {
                 </>
                 )}
                 decimalScale={2}
-                value={getBasketTotal(basket)} // Part of the homework
+                value={0} // Part of the homework
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
             />
+            <button>Proceed to Checkout</button>
         </div>
     )
 }
